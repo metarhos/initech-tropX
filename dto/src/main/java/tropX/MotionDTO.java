4f5a589a1eb94529938dbd5bda3120e4848ce5da
metarhos <GittyAyaya7%>
2021-01-25 09:57:29 +0200

    create the main

---
+++ dto/src/main/java/tropX/MotionDTO.java
@@ -0,0 +1,42 @@
+package tropX;
+
+
+import java.util.Arrays;
+
+public class MotionDTO {
+    private String exerciseName;
+    private int idSensor;
+    private double[][] motionMatrix;
+
+    public MotionDTO() {
+    }
+
+    public MotionDTO(String exerciseName, int idSensor, double[][] motionMatrix) {
+        this.exerciseName = exerciseName;
+        this.idSensor = idSensor;
+        this.motionMatrix = motionMatrix;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public int getIdSensor() {
+        return idSensor;
+    }
+
+    public double[][] getMotionMatrix() {
+        return motionMatrix;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Motion{" +
+                "exerciseName=" + exerciseName +
+                ", idSensor=" + idSensor +
+                ", motionMatrix=" + Arrays.toString(motionMatrix) +
+                '}';
+    }
+}
---
+++ dto/src/main/java/tropX/SpecificationDTO.java
@@ -0,0 +1,47 @@
+package tropX;
+
+import java.util.Arrays;
+
+public class SpecificationDTO {
+
+    private String exerciseName;
+    private int idSensor;
+    private String strategy;
+    private double[][] specMatrix;
+    public SpecificationDTO(String exerciseName, int idSensor, String strategy,double[][] specMatrix) {
+        this.exerciseName = exerciseName;
+        this.idSensor = idSensor;
+        this.strategy = strategy;
+        this.specMatrix = specMatrix;
+    }
+
+    public SpecificationDTO() {
+    }
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "idExample=" + exerciseName +
+                ", idSensor=" + idSensor +
+                ", strategy='" + strategy + '\'' +
+                ", specMatrix=" + Arrays.toString(specMatrix) +
+                '}';
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public int getIdSensor() {
+        return idSensor;
+    }
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    public double[][] getSpecMatrix() {
+        return specMatrix;
+    }
+
+}
